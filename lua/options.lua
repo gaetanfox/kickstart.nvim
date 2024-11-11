27cfa91892e5c69a53365a9d9ee21ef3b06781f1
Gaetan Fox <me@gaetanfox.io>
2024-11-11 22:06:12 +0100

    modified so that diagnostics only appears when leader+q and not in live view

--- lua/options.lua
+++ lua/options.lua
@@ -72,8 +72,8 @@ vim.diagnostic.config { virtual_text = false }
 vim.diagnostic.config { virtual_lines = false }
 
 -- Show line diagnostics automatically in hover window
-vim.o.updatetime = 250
-vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+-- vim.o.updatetime = 250
+-- vim.cmd [[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
 
 -- Hide deprecation warnings
 vim.g.deprecation_warnings = false
