33b8fc4db9e04bc3f18d5284a64ab11de6c148a5
Gaetan Fox <me@gaetanfox.io>
2025-04-01 11:06:37 +0200

    nah

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -4,12 +4,22 @@ return {
   lazy = false,
   version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
   opts = {
-    ---@alias Provider  "openai" | "gemini"  | "copilot" | string
-    provider = 'copilot', -- Recommend using Claude
+    ---@alias Provider  "openai" | "gemini"  | "copilot" | "copilot_claude" | string
+    provider = 'copilot', -- Using Claude 3.7 Sonnet
     -- WARNING: Since auto-suggestions are a high-frequency operation and therefore expensive,
     -- currently designating it as `copilot` provider is dangerous because: https://github.com/yetone/avante.nvim/issues/1048
     -- Of course, you can reduce the request frequency by increasing `suggestion.debounce`.
-    auto_suggestions_provider = 'copilot',
+    openai = {
+      hide_in_model_selector = true,
+    },
+    vendors = {
+      copilot_claude = {
+        __inherited_from = 'copilot',
+        model = 'claude-3.5-sonnet',
+        hide_in_model_selector = true,
+      },
+    },
+    auto_suggestions_provider = 'copilot_claude',
     -- claude = {
     --   endpoint = 'https://api.anthropic.com',
     --   model = 'claude-3-5-sonnet-20241022',
@@ -19,9 +29,6 @@ return {
     -- add any opts here
     -- for example
     -- provider = 'openai',
-    openai = {
-      model = 'gpt-4o-mini', -- your desired model (or use gpt-4o, etc.)
-    },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = 'make',
